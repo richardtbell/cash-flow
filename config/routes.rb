c1442bdd4109a80a0c8a66b4e3e544abb9483203
Richard <richard.t.bell90@gmail.com>
2016-01-24 23:02:15 +0000

    Add root route that will got to the quiz

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :results
   resources :users
+  get '/', to: 'quiz#index'
   get '/quiz', to: 'quiz#index'
   get '/getQuestionData.json', to: 'quiz#get_questions'
 end
